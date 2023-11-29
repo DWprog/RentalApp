1c6ddfb78b230c32c236a4812a630560323a6af0
DWprog <dawid.window@gmail.com>
2023-11-29 14:42:35 +0100

    Add project files.

---
+++ RentalApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RentalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World,Rental Company here!");
+        }
+    }
+}
